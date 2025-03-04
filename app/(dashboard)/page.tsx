ce0012752523a472823d71985bcfdfc3d1aeca81
JuanPa <pinza153@hotmail.com>
2025-03-03 22:02:56 -0500

    Authentication with Clerk

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+return(
+  <>
+  <Button variant={"secondary"}>This is an authenticated route</Button>
+  </>
+)
+
+}
